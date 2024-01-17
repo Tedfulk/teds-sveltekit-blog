8d56a724923ff36cc56462b3fcd48ee658f391ea
Tedfulk <teddyfulk@gmail.com>
2024-01-17 18:34:25 -0500

    fix: updated site-preview

--- src/lib/data/meta.ts
+++ src/lib/data/meta.ts
@@ -2,20 +2,18 @@
 // So they can be added as suffixes on different pages
 // Via <svelte:head>
 
-export const siteBaseUrl = 'https://sveltekit-static-blog-template.vercel.app/';
+export const siteBaseUrl = 'https://teds-tech-chronicles.netlify.app/';
 
 export const keywords = [
 	'Svelte',
 	'SvelteKit',
-	'Template',
 	'Blog',
-	'Starter',
 	'Static Site'
 ];
 
 export const description =
-	"A light, neat, and easy-to-use SvelteKit template for your next website.";
+	"A blog about my journey through the world of web development.";
 
-export const title = 'SvelteKit Static Blog Template';
+export const title = 'Ted\'s Tech Chronicles';
 
 export const image = `${siteBaseUrl}/images/site-preview.png`;
